b655e6744e4dc4c8c195684d0aeca33199d11ef3
ranunclulus <morion002@gmail.com>
2023-04-20 15:26:55 +0900

    Scanner로 바로 변수를 초기화해서 더하는 예제

---
+++ src/main/java/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,11 @@
+package week1.day4;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        // 선언과 초기화 동시에
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+    }
+}
+
+
---
+++ src/main/java/week1/day4/ScannerAndVariable.java
@@ -0,0 +1,12 @@
+package week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerAndVariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println(val1 + val2);
+    }
+}
