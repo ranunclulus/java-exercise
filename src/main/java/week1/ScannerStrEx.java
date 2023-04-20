2db0f8f75cfe88b335560025b52435dce99ad1a9
ranunclulus <morion002@gmail.com>
2023-04-20 09:40:53 +0900

    ScannerStrExTest - Scanner next()로 문자 받아 와서 두 개 더한 뒤 출력하는 예제

---
+++ src/main/java/week1/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    void inputStringAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ src/main/java/week1/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringAndPlus();
+    }
+}
