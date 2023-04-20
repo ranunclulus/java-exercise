9d244073d03ccc7fbbbf7503a5c8e9ba61eb8738
ranunclulus <morion002@gmail.com>
2023-04-20 14:37:04 +0900

    변수 예제: int형 변수 두 개를 입력받아 더하기 출력

---
+++ src/main/java/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,14 @@
+package week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -127;
+        byte byte3 = -128;
+        byte byte4 = 127;
+        System.out.println(byte1);
+        System.out.println(byte2);
+        System.out.println(byte3);
+        System.out.println(byte4);
+    }
+}
