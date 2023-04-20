23b765bf5bf56ccb0c0f8cace7f1bf97ec4d2232
ranunclulus <morion002@gmail.com>
2023-04-20 10:42:04 +0900

    ScannerEx2 - 입력값에 대한 안내를 작성하는 스캐너 예제

--- src/main/java/week1/day4/ScannerExRE.java
+++ src/main/java/week1/day4/ScannerExRE.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ScannerExRE {
     public void readTwoNumbersPlus() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("두 개의 숫자를 각각 입력하고 엔터를 누르세요");
         System.out.println(sc.nextInt() + sc.nextInt());
     }
 }
