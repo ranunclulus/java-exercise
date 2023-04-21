8f15da808d89bb69299c9aa0e53be339f614a944
ranunclulus <morion002@gmail.com>
2023-04-21 11:44:35 +0900

    지역 변수 연습

---
+++ src/main/java/week1/day5/SpaceInvaders.java
@@ -0,0 +1,12 @@
+package week1.day5;
+
+public class SpaceInvaders {
+    private void moveLeft() {
+        int location = 0;
+        location = -1;
+    }
+    private void moveRight() {
+        int location = 0;
+        location = 1;
+    }
+}
