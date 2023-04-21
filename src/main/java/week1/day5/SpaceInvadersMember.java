5fc209b422e35c4502b1c487625777bef6c6bd05
ranunclulus <morion002@gmail.com>
2023-04-21 13:22:52 +0900

    멤버 변수 연습

---
+++ src/main/java/week1/day5/SpaceInvadersMember.java
@@ -0,0 +1,23 @@
+package week1.day5;
+
+public class SpaceInvadersMember {
+    int location; // 멤버변수로 선언
+    public void moveLeft() {
+        location = location - 1;
+        System.out.printf("moveLeft: %d\n", location);
+    }
+    public void meveRight() {
+        location = location + 1;
+        System.out.printf("moveRight: %d\n", location);
+    }
+    public static void main(String[] args) {
+        SpaceInvadersMember sp = new SpaceInvadersMember();
+        sp.moveLeft();
+        sp.moveLeft();
+        sp.meveRight();
+        sp.meveRight();
+        sp.moveLeft();
+        sp.meveRight();
+        sp.meveRight();
+    }
+}
