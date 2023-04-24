a413a8bb1ac148026390ad110a8b0565b0e5f785
ranunclulus <morion002@gmail.com>
2023-04-24 15:39:57 +0900

    code up exercise

---
+++ src/main/java/CodeUp/CodeUp1023.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1023 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] array = sc.next().split("[.]");
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+    }
+}
