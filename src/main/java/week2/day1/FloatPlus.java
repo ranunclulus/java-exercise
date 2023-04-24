749de184d8e76f47d3f49961de6862ec87823bc6
ranunclulus <morion002@gmail.com>
2023-04-24 13:36:54 +0900

    캐스팅 예제

---
+++ src/main/java/week2/day1/FloatPlus.java
@@ -0,0 +1,11 @@
+package week2.day1;
+
+public class FloatPlus {
+    public static void main(String[] args) {
+        String val1 = "1.111111";
+        String val2 = "2.111111";
+        float result = Float.parseFloat(val1) + Float.parseFloat(val2);
+        System.out.println("result = " + result);
+    }
+
+}
---
+++ src/main/java/week2/day1/NarrowCasting.java
@@ -0,0 +1,10 @@
+package week2.day1;
+
+public class NarrowCasting {
+    public static void main(String[] args) {
+        double dVal = 123.456;
+        int iVal = (int) dVal;
+        System.out.println("iVal = " + iVal);
+        System.out.println("dVal = " + dVal);
+    }
+}
