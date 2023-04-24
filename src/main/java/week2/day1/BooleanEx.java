665cf282d585731b4b373b5ef916983935eed8d2
ranunclulus <morion002@gmail.com>
2023-04-24 14:34:42 +0900

    type example

---
+++ src/main/java/week2/day1/BooleanEx.java
@@ -0,0 +1,13 @@
+package week2.day1;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = false;
+
+        System.out.println(flag1 == flag2);
+    }
+}
---
+++ src/main/java/week2/day1/DoublePlus.java
@@ -0,0 +1,10 @@
+package week2.day1;
+
+public class DoublePlus {
+    public static void main(String[] args) {
+        String val1 = "1.11111111";
+        String val2 = "2.11111111";
+        Double result = Double.parseDouble(val1) + Double.parseDouble(val2);
+        System.out.println("result = " + result);
+    }
+}
