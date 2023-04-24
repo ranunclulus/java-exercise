100dcd7b97fbe0adc43d6830f2ce01305dcb726f
ranunclulus <morion002@gmail.com>
2023-04-24 10:27:22 +0900

    int와 float 자료형 연습

---
+++ src/main/java/week2/day1/DevideintoTwo.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class DevideintoTwo {
+    public static void main(String[] args) {
+        float fOne = 1;
+        float fTwo = 2;
+        int iOne = 1;
+        int iTwo = 2;
+        System.out.printf("Float %f\n", fOne + fOne / fTwo);
+        System.out.printf("Int %d\n", iOne + iOne / iTwo);
+        System.out.printf("Float %f\n", 9 / 2.0);
+        System.out.printf("Int %d\n", 9 / 2);
+    }
+}
