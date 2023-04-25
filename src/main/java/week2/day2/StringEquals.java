596108eed4e7515c4b743aabb25cd6db77dfe49b
ranunclulus <morion002@gmail.com>
2023-04-25 11:48:33 +0900

    string equl example

---
+++ src/main/java/week2/day2/StringEquals.java
@@ -0,0 +1,8 @@
+package week2.day2;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        System.out.println("EQUL" == "EQUL");
+        // String은 똑같은 게 있으면 하나만 생성하고 두 개 만들지 않음
+    }
+}
