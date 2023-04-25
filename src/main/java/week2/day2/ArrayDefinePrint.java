88d3df6df90c894471935be6162bbcdd195665f4
ranunclulus <morion002@gmail.com>
2023-04-25 14:28:37 +0900

    array exercise

---
+++ src/main/java/week2/day2/ArrayDefinePrint.java
@@ -0,0 +1,11 @@
+package week2.day2;
+
+public class ArrayDefinePrint {
+    public static void main(String[] args) {
+        int[] iArr = new int[] {1, 2, 3, 8};
+        System.out.println("iArr[0] = " + iArr[0]);
+        System.out.println("iArr[1] = " + iArr[1]);
+        System.out.println("iArr[2] = " + iArr[2]);
+        System.out.println("iArr[3] = " + iArr[3]);
+    }
+}
---
+++ src/main/java/week2/day2/DefAndInitArr.java
@@ -0,0 +1,16 @@
+package week2.day2;
+
+public class DefAndInitArr {
+    public static void main(String[] args) {
+        int[] iArr = new int[5];
+        iArr[0] = 2;
+        iArr[1] = 4;
+        iArr[2] = 7;
+        System.out.println("iArr[0] = " + iArr[0]);
+        System.out.println("iArr[1] = " + iArr[1]);
+        System.out.println("iArr[2] = " + iArr[2]);
+        System.out.println("iArr[3] = " + iArr[3]);
+        System.out.println("iArr[4] = " + iArr[4]);
+    }
+    
+}
