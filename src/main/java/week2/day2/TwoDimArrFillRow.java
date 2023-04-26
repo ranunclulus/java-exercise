0d647c346c3e2d54343b5ecd81f327e06eeb6723
ranunclulus <morion002@gmail.com>
2023-04-26 13:17:09 +0900

    array exercise

---
+++ src/main/java/week2/day2/TwoDimArrFillRow.java
@@ -0,0 +1,27 @@
+package week2.day2;
+
+import java.util.Arrays;
+
+public class TwoDimArrFillRow {
+    public static void fillARow(int[][] arr, int rowNum) {
+        arr[rowNum][0] = 1;
+        arr[rowNum][1] = 1;
+        arr[rowNum][2] = 1;
+        arr[rowNum][3] = 1;
+        arr[rowNum][4] = 1;
+        printTdimArr(arr);
+    }
+
+    public static void printTdimArr(int[][] arr) {
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.println(Arrays.toString(arr[3]));
+        System.out.println(Arrays.toString(arr[4]));
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        fillARow(arr, 3);
+    }
+}
