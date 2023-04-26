5ec340b616d46e29bd2f1f2b33252e280e92ce85
ranunclulus <morion002@gmail.com>
2023-04-26 15:37:12 +0900

    like accumulate exercise

---
+++ src/main/java/week2/day3/LikeAccumulate.java
@@ -0,0 +1,11 @@
+package week2.day3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeAcount = 1;
+        likeAcount += 1;
+        System.out.println("likeAcount = " + likeAcount);
+        likeAcount -= 1;
+        System.out.println("likeAcount = " + likeAcount);
+    }
+}
