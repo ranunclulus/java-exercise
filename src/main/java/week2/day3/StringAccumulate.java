1c4e83c00ebbcdac83edd74fc3496a384ee134cf
ranunclulus <morion002@gmail.com>
2023-04-26 15:42:36 +0900

    string accumulate

---
+++ src/main/java/week2/day3/StringAccumulate.java
@@ -0,0 +1,11 @@
+package week2.day3;
+
+public class StringAccumulate {
+    public static void main(String[] args) {
+        String answer = "";
+        answer += "쿵";
+        answer += "짝";
+        answer += "쿵";
+        System.out.println("answer = " + answer);
+    }
+}
