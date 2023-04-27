00f9ccb0a05eb2fa5f6d28f81b51dd4b66e953c1
ranunclulus <morion002@gmail.com>
2023-04-27 09:22:48 +0900

    create: array sum example

---
+++ src/main/java/week2/day4/ArraySum.java
@@ -0,0 +1,13 @@
+package week2.day4;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] array = {2, 1, 7, 4};
+        int result = array[0];
+        result = result + array[1];
+        result = result + array[2];
+        result = result + array[3];
+        System.out.println("result = " + result);
+        
+    }
+}
