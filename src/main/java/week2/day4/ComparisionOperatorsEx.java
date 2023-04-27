00e6cc4aa2e7ad779ea1a9bac264ec92475f034b
ranunclulus <morion002@gmail.com>
2023-04-27 10:38:34 +0900

    create: comparision operator example

---
+++ src/main/java/week2/day4/ComparisionOperatorsEx.java
@@ -0,0 +1,14 @@
+package week2.day4;
+
+public class ComparisionOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+        System.out.printf("iVal1 == iBal2 --> %b\n", iVal1 == iVal2);
+        System.out.printf("iVal1 != iBal2 --> %b\n", iVal1 != iVal2);
+        System.out.printf("iVal1 < iBal2 --> %b\n", iVal1 < iVal2);
+        System.out.printf("iVal1 <= iBal2 --> %b\n", iVal1 <= iVal2);
+        System.out.printf("iVal1 > iBal2 --> %b\n", iVal1 > iVal2);
+        System.out.printf("iVal1 >= iBal2 --> %b\n", iVal1 >= iVal2);
+    }
+}
