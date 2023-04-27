3ad7a3f1fcddf007f93678ca66cca1b4b1c2acee
ranunclulus <morion002@gmail.com>
2023-04-27 14:44:28 +0900

    feat: bit operation not example

---
+++ src/main/java/week2/day4/BitOperationEx2.java
@@ -0,0 +1,11 @@
+package week2.day4;
+
+public class BitOperationEx2 {
+    public static void main(String[] args) {
+        int iVal = 1;
+        System.out.println(iVal);
+        System.out.println(Integer.toBinaryString(iVal));
+        System.out.println(~iVal);
+        System.out.println(Integer.toBinaryString(~iVal));
+    }
+}
