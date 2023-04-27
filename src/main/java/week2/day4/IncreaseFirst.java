a2be5ec2e89fa4e9b882c7403de2e7bf1e6607e4
ranunclulus <morion002@gmail.com>
2023-04-27 10:12:43 +0900

    create: increase first example

---
+++ src/main/java/week2/day4/IncreaseFirst.java
@@ -0,0 +1,12 @@
+package week2.day4;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 6};
+        System.out.println(arr[idx++]);
+        System.out.println(arr[idx++]);
+        System.out.println(arr[idx++]);
+        System.out.println(arr[idx++]);
+    }
+}
