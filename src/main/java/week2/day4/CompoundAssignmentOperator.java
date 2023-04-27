a47cd28bb4fba66f14c38a3f821792b9af9f3222
ranunclulus <morion002@gmail.com>
2023-04-27 09:31:40 +0900

    create: compound assignment operator example

---
+++ src/main/java/week2/day4/CompoundAssignmentOperator.java
@@ -0,0 +1,9 @@
+package week2.day4;
+
+public class CompoundAssignmentOperator {
+    public static void main(String[] args) {
+        int x = 10;
+        x += 1;
+        System.out.println("x = " + x);
+    }
+}
