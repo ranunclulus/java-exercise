57a8c882160cf675ae5dd069efde660b19331c84
ranunclulus <morion002@gmail.com>
2023-04-28 10:16:32 +0900

    feat: else if example

---
+++ src/main/java/week2/day5/IfWrong.java
@@ -0,0 +1,16 @@
+package week2.day5;
+
+public class IfWrong {
+    public static void main(String[] args) {
+        int score = 81;
+        if (score >= 90) {
+            System.out.println("A");
+        }
+        else if (score >= 80) {
+            System.out.println("B");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+    }
+}
