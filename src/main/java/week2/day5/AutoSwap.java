feb823b2376aa2684f52c6d75ea4e946f59bee9e
ranunclulus <morion002@gmail.com>
2023-04-28 11:05:07 +0900

    feat: autoswap example

---
+++ src/main/java/week2/day5/AutoSwap.java
@@ -0,0 +1,17 @@
+package week2.day5;
+
+import java.util.Arrays;
+
+public class AutoSwap {
+    public static void main(String[] args) {
+        // 오름차순 정렬
+        int[] arr = {2, 1, 7, 9};
+        boolean check = arr[0] > arr[1];
+        if (check) {
+            int temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
