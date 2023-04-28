6673bda0492675f3cef5e75d7d46b467113364f2
ranunclulus <morion002@gmail.com>
2023-04-28 11:27:31 +0900

    feat: switch case day of week example

---
+++ src/main/java/week2/day5/SwitchCaseDayOfWeek.java
@@ -0,0 +1,29 @@
+package week2.day5;
+
+public class SwitchCaseDayOfWeek {
+    public static void main(String[] args) {
+        int dayOfWeek = 4;
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("mon");
+                break;
+            case 2:
+                System.out.println("tue");
+                break;
+            case 3:
+                System.out.println("wed");
+                break;
+            case 4:
+                System.out.println("thu");
+                break;
+            case 5:
+                System.out.println("fri");
+                break;
+            case 6:
+                System.out.println("sat");
+                break;
+            case 7:
+                System.out.println("sun");
+        }
+    }
+}
