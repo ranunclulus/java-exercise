7f5b9325e2b79d757ec467212f0e586b9170a638
ranunclulus <morion002@gmail.com>
2023-05-01 14:18:40 +0900

    feat: multiplication table example

---
+++ src/main/java/week4/day1/MultiplicatoinTable.java
@@ -0,0 +1,9 @@
+package week4.day1;
+
+public class MultiplicatoinTable {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 9; i++) {
+            System.out.printf("2 * %d = %d\n", i, i * 2);
+        }
+    }
+}
