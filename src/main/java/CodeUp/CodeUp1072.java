f098d62f8cd8dd84863c880ab1796233e276ec96
ranunclulus <morion002@gmail.com>
2023-05-01 10:49:44 +0900

    feat: codeup exercise 1072

---
+++ src/main/java/CodeUp/CodeUp1072.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt();
+        for (int i = 0; i < iVal; i ++) {
+            System.out.println(i + 1);
+        }
+    }
+}
