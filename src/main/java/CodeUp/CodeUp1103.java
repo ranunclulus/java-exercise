1713162fb2a2c91a772bf3ef36156d8dd7a51622
ranunclulus <morion002@gmail.com>
2023-05-01 09:18:28 +0900

    feat: codeup exercise 1103, 1106

---
+++ src/main/java/CodeUp/CodeUp1103.java
@@ -0,0 +1,7 @@
+package CodeUp;
+
+public class CodeUp1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/CodeUp/CodeUp1106.java
@@ -0,0 +1,9 @@
+package CodeUp;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        int mInt = -2147483648;
+        int pInt = 2147483647;
+        System.out.printf("%d %d", mInt, pInt);
+    }
+}
