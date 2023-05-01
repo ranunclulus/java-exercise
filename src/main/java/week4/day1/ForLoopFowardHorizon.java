736d845ffaaf4d5606ba78b434081d5158328e68
ranunclulus <morion002@gmail.com>
2023-05-01 10:17:15 +0900

    feat: for loop foward horizon example

---
+++ src/main/java/week4/day1/ForLoopFowardHorizon.java
@@ -0,0 +1,9 @@
+package week4.day1;
+
+public class ForLoopFowardHorizon {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i ++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
