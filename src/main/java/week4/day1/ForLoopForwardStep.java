f1dd24d1eaf98302b0544ba690ec7c4b9f1c652a
ranunclulus <morion002@gmail.com>
2023-05-01 13:12:56 +0900

    feat: for loop forward step

---
+++ src/main/java/week4/day1/ForLoopForwardStep.java
@@ -0,0 +1,9 @@
+package week4.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
