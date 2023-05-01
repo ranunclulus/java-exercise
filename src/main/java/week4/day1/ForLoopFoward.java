251b6123f0fddcaccbe236735e0bcac0c06e8e40
ranunclulus <morion002@gmail.com>
2023-05-01 10:08:43 +0900

    feat: for loop foward

---
+++ src/main/java/week4/day1/ForLoopFoward.java
@@ -0,0 +1,9 @@
+package week4.day1;
+
+public class ForLoopFoward {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i ++) {
+            System.out.println(i);
+        }
+    }
+}
