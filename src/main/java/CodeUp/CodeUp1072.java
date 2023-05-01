de89534662149d42a1e186830483143e1de562ce
ranunclulus <morion002@gmail.com>
2023-05-01 10:56:45 +0900

    fix: codeup exercise 1072

--- src/main/java/CodeUp/CodeUp1072.java
+++ src/main/java/CodeUp/CodeUp1072.java
@@ -6,8 +6,12 @@ public class CodeUp1072 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int iVal = sc.nextInt();
+        int[] array = new int[iVal];
         for (int i = 0; i < iVal; i ++) {
-            System.out.println(i + 1);
+            array[i] = sc.nextInt();
+        }
+        for (int i = 0; i < iVal; i++){
+            System.out.println(array[i]);
         }
     }
 }
