fd21260a5447695970a820bd87887a6f1eaf80eb
ranunclulus <morion002@gmail.com>
2023-05-08 10:16:20 +0900

    feat: right triangle example

---
+++ src/main/java/week4/day1/RightTriangle.java
@@ -0,0 +1,16 @@
+package week4.day1;
+
+import java.util.Scanner;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
