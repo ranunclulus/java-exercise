d4e85550ba9c1e5f711a72a6417da96c2d215d30
ranunclulus <morion002@gmail.com>
2023-05-09 10:21:53 +0900

    fix: reverse pyramid with constructor

--- src/main/java/week4/day2/ReversePyramid.java
+++ src/main/java/week4/day2/ReversePyramid.java
@@ -1,14 +1,22 @@
 package week4.day2;
 
 public class ReversePyramid {
+    private String spaceChar = "";
+    public ReversePyramid(String spaceChar) {
+        this.spaceChar = spaceChar;
+    }
     public String makeALine(int height, int i) {
-        return String.format("%s%s\n", " ".repeat(i), "*".repeat(height + 3 - (i * 2)));
+        return String.format("%s%s\n", spaceChar.repeat(i), "*".repeat(height + 3 - (i * 2)));
     }
     public static void main(String[] args) {
         int height = 4;
-        ReversePyramid reversePyramid = new ReversePyramid();
+        ReversePyramid reversePyramid = new ReversePyramid(" ");
         for (int i = 0; i < height; i++) {
             System.out.println(reversePyramid.makeALine(height, i));
+        }
+        ReversePyramid reversePyramidZero = new ReversePyramid("0");
+        for (int i = 0; i < height; i++) {
+            System.out.println(reversePyramidZero.makeALine(height, i));
         }
     }
 }
