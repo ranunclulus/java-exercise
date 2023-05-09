4952ee2726ae7aa582f40917963ba1ae7a6de11c
ranunclulus <morion002@gmail.com>
2023-05-09 09:18:18 +0900

    feat: object oriented pyramid

---
+++ src/main/java/week4/day2/Pyramid2.java
@@ -0,0 +1,20 @@
+package week4.day2;
+
+public class Pyramid2 {
+    private String spaceChar = " ";
+    public Pyramid2(String spaceChar) {
+        this.spaceChar = spaceChar;
+    }
+    public void printPyramid(int height) {
+        for (int i = 0; i < height; i++) {
+            System.out.printf("%s%s\n", spaceChar.repeat(height - i - 1), "*".repeat(2 * i + 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Pyramid2 pyramid2 = new Pyramid2(" ");
+        pyramid2.printPyramid(4);
+        Pyramid2 pyramidZero = new Pyramid2("0");
+        pyramidZero.printPyramid(4);
+    }
+}
