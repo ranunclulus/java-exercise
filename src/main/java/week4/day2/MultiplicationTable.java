62d143e7e4126d8125169e22d75311e708dd4d2c
ranunclulus <morion002@gmail.com>
2023-05-09 13:11:13 +0900

    fix: multiplication refactoring

--- src/main/java/week4/day2/MultiplicationTable.java
+++ src/main/java/week4/day2/MultiplicationTable.java
@@ -1,14 +1,18 @@
 package week4.day2;
 
 public class MultiplicationTable {
-    public static void printDan(String multiplesSymbol, int dan) {
+    private String multipleSymbol;
+    public MultiplicationTable(String multipleSymbol) {
+        this.multipleSymbol = multipleSymbol;
+    }
+    public void printDan(int dan) {
         for (int i = 1; i <= 9; i++) {
-            System.out.printf("%d %s %d = %d\n", dan, multiplesSymbol, i, dan * i);
+            System.out.printf("%d %s %d = %d\n", dan, multipleSymbol, i, dan * i);
         }
         System.out.println("------------");
     }
     public static void main(String[] args) {
-        printDan("*",2);
-        printDan("x",9);
+        MultiplicationTable mt = new MultiplicationTable("X");
+        mt.printDan(3);
     }
 }
