20f5baa22bed74f07f4f1e90a7002796ac6906b6
ranunclulus <morion002@gmail.com>
2023-05-09 09:50:41 +0900

    fix: method add

--- src/main/java/week4/day2/Pyramid2.java
+++ src/main/java/week4/day2/Pyramid2.java
@@ -11,6 +11,10 @@ public class Pyramid2 {
         }
     }
 
+    public String makeALine(String spaceChar, int height, int i) {
+        return String.format("%s%s\n", spaceChar.repeat(height - i - 1), "*".repeat(2 * i + 1));
+    }
+
     public static void main(String[] args) {
         Pyramid2 pyramid2 = new Pyramid2(" ");
         pyramid2.printPyramid(4);
