796eecbbbaacbe3429e480a6258fe6bd736b35c8
ranunclulus <morion002@gmail.com>
2023-05-10 09:47:19 +0900

    fix: add no repeat string

--- src/main/java/week4/day3/DrawDiamond.java
+++ src/main/java/week4/day3/DrawDiamond.java
@@ -5,6 +5,14 @@ public class DrawDiamond {
         return symbol.repeat(repeatNum);
     }
 
+    public static String getNoRepeatedSymbol(String symbol, int repeatNum) {
+        String result = "";
+        for (int i = 0; i < repeatNum; i++) {
+            result += symbol;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int h = 7;
         int pivot = h / 2;
@@ -15,6 +23,15 @@ public class DrawDiamond {
             } else {
                 //System.out.printf("%d %d\n", i - pivot, -2 * i + 2 * pivot + h);
                 System.out.printf("%s%s\n", getRepeatedSymbol(" ", i - pivot), getRepeatedSymbol("*", -2 * i + 2 * pivot + h));
+            }
+        }
+        for (int i = 0; i < h; i++) {
+            if(i <= pivot) {
+                //System.out.printf("%d %d\n", pivot - i, 2 * i + 1);
+                System.out.printf("%s%s\n", getNoRepeatedSymbol(" ", pivot - i), getNoRepeatedSymbol("*", 2 * i + 1));
+            } else {
+                //System.out.printf("%d %d\n", i - pivot, -2 * i + 2 * pivot + h);
+                System.out.printf("%s%s\n", getNoRepeatedSymbol(" ", i - pivot), getNoRepeatedSymbol("*", -2 * i + 2 * pivot + h));
             }
         }
     }
