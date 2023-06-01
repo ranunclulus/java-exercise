33cc1ae3032ee55372428551260bea63af0f2d5a
ranunclulus <morion002@gmail.com>
2023-06-01 09:22:56 +0900

    feat: random val

---
+++ src/main/java/week7/day4/Random.java
@@ -0,0 +1,8 @@
+package week7.day4;
+
+public class Random {
+    public static void main(String[] args) {
+        int a = (int)(Math.random() * 10);
+        int b = (int)(Math.random() * (10 - a));
+    }
+}
