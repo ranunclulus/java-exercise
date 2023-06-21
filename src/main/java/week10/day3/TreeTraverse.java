d2153e8c50710f02f5f5d5d76e9d56ec02bbae17
ranunclulus <morion002@gmail.com>
2023-06-21 10:46:31 +0900

    feat: postorder

--- src/main/java/week10/day3/TreeTraverse.java
+++ src/main/java/week10/day3/TreeTraverse.java
@@ -28,11 +28,21 @@ public class TreeTraverse {
         }
     }
 
+    public void traversePostorder(int node) {
+        if(node < this.nodes && arr[node] != 0) {
+            this.traversePostorder(node * 2);
+            this.traversePostorder(node * 2 + 1);
+            System.out.print(arr[node] + ", ");
+        }
+    }
+
     public static void main(String[] args) {
         TreeTraverse tree = new TreeTraverse();
         tree.setArr(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
         tree.traversePreorder(1);
         System.out.println();
         tree.traverseInorder(1);
+        System.out.println();
+        tree.traversePostorder(1);
     }
 }
