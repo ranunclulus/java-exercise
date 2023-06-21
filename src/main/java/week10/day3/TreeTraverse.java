1ba16db651be5b85d766517730f2d2f3e48a2eb2
ranunclulus <morion002@gmail.com>
2023-06-21 10:43:41 +0900

    feat: inorder

--- src/main/java/week10/day3/TreeTraverse.java
+++ src/main/java/week10/day3/TreeTraverse.java
@@ -11,10 +11,28 @@ public class TreeTraverse {
     }
 
     // 전위 순회 V -> L -> R
-    // predorder(): V -> preorder(L) -> preorder(R)
+    // preorder(): System.out.print(V) -> preorder(L) -> preorder(R)
     public void traversePreorder(int node) {
-        System.out.println(arr[node] + ", "); // 방문
-        this.traversePreorder(node * 2); // 왼쪽 노드를 기준으로 다시 호출
-        this.traversePreorder(node * 2 + 1); // 오른쪽 노드를 기준으로 다시 호출
+        if (node < this.nodes && arr[node] != 0) {
+            System.out.print(arr[node] + ", ");   // 방문
+            this.traversePreorder(node * 2);      // 왼쪽 자식(i * 2)을 루트로 다시 preorder 호출
+            this.traversePreorder(node * 2 + 1);  // 오른쪽 자식(i * 2 + 1)을 루트로 다시 preorder 호출
+        }
+    }
+
+    public void traverseInorder(int node) {
+        if(node < this.nodes && arr[node] != 0) {
+            this.traverseInorder(node * 2);;
+            System.out.print(arr[node] + ", ");
+            this.traverseInorder(node * 2 + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeTraverse tree = new TreeTraverse();
+        tree.setArr(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+        tree.traversePreorder(1);
+        System.out.println();
+        tree.traverseInorder(1);
     }
 }
