96fe6822866e48d845328817968de1b142864e7a
ranunclulus <morion002@gmail.com>
2023-06-26 11:02:52 +0900

    feat: adj list update

--- src/main/java/week11/day1/AdjacentList.java
+++ src/main/java/week11/day1/AdjacentList.java
@@ -11,41 +11,51 @@ import java.util.StringTokenizer;
 public class AdjacentList {
     public void solution() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        // StringTokenizer: 입력받은 문자열을 띄어 쓰기 또는 지정된 delimiter를 기준으로 나누어서 한 단어씩 반환해 주는 도구
-        StringTokenizer graphTokenizer // 8 10 입력
+        // StringTokenizer: 입력받은 문자열을 ' ' (또는 지정된 delimiter) 를 기준으로 나누어서
+        // 한 단어씩 반환해주는 도구
+        StringTokenizer graphTokenizer  // 8 10
                 = new StringTokenizer(reader.readLine());
         // StringTokenizer.nextToken(): 다음 단어를 가져오기
-        int maxNode = Integer.parseInt(graphTokenizer.nextToken());
-        int edges = Integer.parseInt(graphTokenizer.nextToken());
-        // 안쪽의 List<Integer> 가 maxNode의 길이를 반드시 가지지는 않을 것이다.
-        List<List<Integer>> adjList = new ArrayList<>();
+        int maxNodes = Integer.parseInt(graphTokenizer.nextToken());  // 8
+        int edges = Integer.parseInt(graphTokenizer.nextToken());     // 10
 
-        // 먼저 List의 내용물을 초기화해 줌
-        for (int i = 0; i < maxNode; i++) {
+        // 안쪽의 List<Integer>가 maxNodes의 길이를 반드시 가지지는 않을 것이다.
+        List<List<Integer>> adjList = new ArrayList<>();
+        // 먼저 리스트의 내용물을 초기화 해준다.
+        for (int i = 0; i < maxNodes; i++) {
             adjList.add(new ArrayList<>());
         }
 
+        // 간선의 갯수만큼 반복해서 입력을 받는다.
         for (int i = 0; i < edges; i++) {
+            // 다음줄을 단어 단위로 나눠주는 Tokenizer
             StringTokenizer edgeTokenizer
                     = new StringTokenizer(reader.readLine());
-            int startNode = Integer.parseInt(edgeTokenizer.nextToken()); // i 입력
-            int endNode = Integer.parseInt(edgeTokenizer.nextToken()); // j 입력
-            // adjList 의 startNode 번째 리스트에 endNode를 첨부한다.
-            // 유향 그래프일 경우
+            // 입력 줄의 첫 숫자
+            int startNode = Integer.parseInt(edgeTokenizer.nextToken());
+            // 입력 줄의 두번째 숫자
+            int endNode = Integer.parseInt(edgeTokenizer.nextToken());
+            // adjList 의 startNode 번째 리스트에 endNode 를 첨부한다.
+            // 유향 그래프일 경우 아래 줄 만
             adjList.get(startNode).add(endNode);
-            // 무향 그래프일 경우
+            // 무향 그래프일 경우 아래 줄도 함께
             adjList.get(endNode).add(startNode);
         }
-        // 선택 사항: DFS/BFS 를 할 때 이왕이면 방문 순서를 작은 숫자부터 방문하도록 지키고 싶다면
-        // 정렬해야 한다
+        // 선택사항: DFS나 BFS를 할때 이왕이면 방문순서를
+        // '작은 숫자부터' 와 같은 조건을 붙이고 싶다면
+        // 정렬해야 한다.
         for (List<Integer> adjRow: adjList) {
+            // 정렬해주는 Collections.sort 메소드
             Collections.sort(adjRow);
         }
 
-        for (List<Integer> adjRow: adjList
-             ) {
+        for (List<Integer> adjRow: adjList) {
             System.out.println(adjRow);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new AdjacentList().solution();
     }
 }
 /*
