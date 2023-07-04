95bfcfef995951f2f10c0c090f40758e2e970581
ranunclulus <morion002@gmail.com>
2023-07-04 10:45:33 +0900

    feat: combination

--- src/main/java/week12/day2/Combination.java
+++ src/main/java/week12/day2/Combination.java
@@ -14,5 +14,18 @@ public class Combination {
                 }
             }
         }
+
+        int[] numbers = {5, 6, 7, 8, 9};
+        for (int i = 0; i < n - 2; i++) {
+            // j도 최소한 하나의 숫자는 남겨 둬야 나머지 한 숫자를 마저 고를 수 있음
+            for (int j = i + 1; j < n - 1; j++) {
+                // k는 선택할 수 있는 나마ㅓ지 중 하나 선택
+                for (int k = j + 1; k < n; k++) {
+                    System.out.printf("%d %d %d \n", numbers[i], numbers[j], numbers[k]);
+                }
+            }
+        }
+
+        // for문의 단점 -> 동적으로 만들기가 어렵다
     }
 }
