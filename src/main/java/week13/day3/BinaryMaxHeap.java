2b01dfa903776f5a52dd576e5917cb61ed26b589
ranunclulus <morion002@gmail.com>
2023-07-12 11:30:03 +0900

    feat: remove heap

--- src/main/java/week13/day3/BinaryMaxHeap.java
+++ src/main/java/week13/day3/BinaryMaxHeap.java
@@ -29,6 +29,54 @@ public class BinaryMaxHeap {
             int temp = heap[index];
             heap[index] = heap[parentIndex];
             heap[parentIndex] = temp;
+            index = parentIndex;
         }
     }
+
+    // 삭제 연산
+    public int remove() {
+        // 루트 노드 제거
+        int root = heap[0];
+        // 마지막 자식 할당
+        heap[0] = heap[size - 1];
+        size--;
+        // TODO 아래로 끌어내리기
+        reHeapDown(0);
+        return root;
+    }
+
+    private void reHeapDown(int index) {
+        while(index < size) {
+            int leftChild = index * 2 + 1;
+            int rightChild = index * 2 + 2;
+            int maxIndex = index;
+            // 왼쪽 자식이 존재하며 왼쪽 자식이 현재 루트보다 클 때
+            if (leftChild < size && heap[leftChild] > heap[maxIndex]) {
+                maxIndex = leftChild;
+            }
+            // 오른쪽이 제일 크다면 갱신되었을 것
+            if (rightChild < size && heap[rightChild] > heap[maxIndex]) {
+                maxIndex = rightChild;
+            }
+
+            // 부모 인덱스가 제일 큰 경우는 힙 만족이라 조건
+            if (maxIndex == index) break;
+
+            int temp = heap[index];
+            heap[index] = heap[maxIndex];
+            heap[maxIndex] = temp;
+            index = maxIndex;
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryMaxHeap binaryMaxHeap = new BinaryMaxHeap();
+        for (int i = 0; i < 32; i++) {
+            binaryMaxHeap.insert(i);
+        }
+        for (int i = 0; i < 32; i++) {
+            System.out.println(binaryMaxHeap.remove());
+        }
+    }
+
 }
