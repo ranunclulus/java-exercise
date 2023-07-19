37530ca8ea97e82bcb7250b0ca98ca0e00edd684
ranunclulus <morion002@gmail.com>
2023-07-19 10:21:26 +0900

    feat: fibonacci with dynamic

--- src/main/java/week14/day3/fibo.java
+++ src/main/java/week14/day3/fibo.java
@@ -2,15 +2,31 @@ package week14.day3;
 
 public class fibo {
     // n을 입력받고 n번째 피보나치 수열 출력
-    public int fiboSimple(int n) {
+    public int fiboRe(int n) {
         if(n == 1 || n == 2) {
             return 1;
         }
         if(n == 0) return 0;
-        return (fiboSimple(n - 1) + fiboSimple(n - 2));
+        return (fiboRe(n - 1) + fiboRe(n - 2));
     }
 
+    public int fiboMemo(int n) {
+        return fiboMemoRe(n, new int[n + 1]);
+    }
+
+    public int fiboMemoRe(int n, int[] memo) {
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+        if(n == 0) return 0;
+        else if (memo[n] == 0) // 만약 아직 없다면 이번에 구해서 기록
+            memo[n] = fiboMemoRe(n - 1, memo) + fiboMemoRe(n - 2, memo);
+        // memo[n] 이 있다면 해당 값이 지금 구하고 있는 n번째 피보나치 수열 값
+        return memo[n];
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new fibo().fiboSimple(10));
+        System.out.println(new fibo().fiboMemo(10));
     }
 }
